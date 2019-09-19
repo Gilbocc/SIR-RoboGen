12aa56dbd4b3427a75d8b0ff6fc77fc6a8556b05
Luca Passeri <luca.passeri3@studio.unibo.it>
2019-09-19 10:48:44 +0200

    Modified parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,29 +1,33 @@
 import json
 
 input_file = "robot.json"
-output_file = "output.json"
+output_file = "output.txt"
 
-brain = json.load(open(input_file))['brain']
+
+brain = json.load(open(input_file))["brain"]
 
 neurons = brain["neuron"]
 connections = brain["connection"]
 
+with open(output_file, "w") as outfile:
+    outfile.write("\n")
+
 with open(output_file, "a") as outfile:
 
-    print("\n")
+    outfile.write("\n")
     for connection in connections:
         src = connection["src"].split("-")
         dest = connection["dest"].split("-")
         weight = connection["weight"]
-        res = src[0] + " " + src[1] + " " + dest[0] + " " + dest[1] + " " + str(weight)
-        print(res)
-        #json.dump(res, outfile)
+        res = src[0] + " " + src[1] + " " + dest[0] + " " + dest[1] + " " + str(weight) + "\n"
+        #print(res)
+        outfile.write(res)
 
-    print("\n")
+    outfile.write("\n")
     for neuron in neurons:
         if (neuron["type"] == "sigmoid"):
             id = neuron["id"].split("-")
             bias = neuron["bias"]
-            res = id[0] + " " + id[1] + " " + str(bias)
-            print(res)
-            #json.dump(res, outfile)
+            res = id[0] + " " + id[1] + " " + str(bias) + "\n"
+            #print(res)
+            outfile.write(res)
